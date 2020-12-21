7184a00377c40682bc659b792d4870e56f92c67c
zhaomiao <949860791@qq.com>
2020-12-21 10:16:38 +0800

    wendy changed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "JoJo changed this page~! " 
+print "JoJo changed this page~! " "\n";
 print "wendy changed this page~! " 
 ?>
